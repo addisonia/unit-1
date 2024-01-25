199819a8bcdc37bfdbfce4ff96b31caed7fbef22
addison atkin <addisonathome@gmail.com>
2024-01-24 21:54:44 -0600

    Added all files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Add all scripts to the JS folder
+
+/*Addison Atkin 2024 */
+
+
+var mydiv = document.getElementById("mydiv");
+mydiv.addEventListener("click", function(){
+    alert("Hello World!");
+});
